700fb45c2779525dbfe9f171455875aea8aebfb7
Rob Britton <rob@robbritton.com>
2025-03-04 07:43:49 -0500

    Local tweaks.

--- .config/nvim/lua/robbrit/lazy/telescope.lua
+++ .config/nvim/lua/robbrit/lazy/telescope.lua
@@ -1,4 +1,18 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	lazy = false,
+	init = function()
+		local telescope = require("telescope.builtin")
+		vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = "Telescope find file" })
+		vim.keymap.set('n', '<leader>fs', telescope.live_grep, { desc = "Telescope search string" })
+
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					hidden = true
+				}
+			}
+		})
+	end
 }
--- .config/nvim/lua/robbrit/lazy/tokyonight.lua
+++ .config/nvim/lua/robbrit/lazy/tokyonight.lua
@@ -1,6 +1,9 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	init = function()
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
